4f6383a905551d9f394f639ff68f1a1ff581a85d
argi-giavris <argi.giavris@gmail.com>
2023-11-28 13:38:59 +0200

    Initial Commit 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,34 +3,46 @@ package org.example;
 import org.example.Models.User;
 import org.example.Utils.DbUtils;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 
 import static org.example.Utils.DbUtils.dataSource;
 
 public class Main {
 
-    User newUser = new User();
+
     public static void main(String[] args) throws SQLException {
 
+        User newUser = new User("example9@email.com", "Test Name9");
+        User newUser2 = new User("example10@email.com", "Test Name10");
+        String query = "Insert into users (email, name) VALUES (?, ?)";
 
         //example to test rollback
         try (Connection sharedConnection = dataSource.getConnection()) {
             DbUtils.inTransactionWithoutResult(sharedConnection, connection -> {
-                try (PreparedStatement statement = connection.prepareStatement("Insert into users (email, name) VALUES (?, ?)")) {
-                    statement.setString(1, "example9@email.com");
-                    statement.setString(2, "Test Name6");
+                try (PreparedStatement statement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS)) {
+                    statement.setString(1, newUser.getEmail());
+                    statement.setString(2, newUser.getName());
                     statement.executeUpdate();
+
+                    try (ResultSet keys = statement.getGeneratedKeys()) {
+                        if (keys.next()) {
+                            newUser.setId(keys.getInt(1));
+                        }
+                    }
 
                 }
 
                 DbUtils.inTransactionWithoutResult(sharedConnection, connection1 -> {
-                    try (PreparedStatement statement = connection1.prepareStatement("Insert into users (email, name) VALUES (?, ?)")) {
-                        statement.setString(1, "example9@email.com");
-                        statement.setString(2, "Test Name6");
+                    try (PreparedStatement statement = connection1.prepareStatement(query, Statement.RETURN_GENERATED_KEYS)) {
+                        statement.setString(1, newUser.getEmail());
+                        statement.setString(2, newUser.getName());
                         statement.executeUpdate();
+
+                        try (ResultSet keys = statement.getGeneratedKeys()) {
+                            if (keys.next()) {
+                                newUser.setId(keys.getInt(1));
+                            }
+                        }
                     }
                 });
             });
@@ -38,12 +50,19 @@ public class Main {
             throw new RuntimeException(e);
         }
 
+
         try (Connection sharedConnection = dataSource.getConnection()) {
             DbUtils.withConnection(sharedConnection, connection -> {
-                try (PreparedStatement statement = connection.prepareStatement("Insert into users (email, name) VALUES (?, ?)")) {
-                    statement.setString(1, "example10@email.com");
-                    statement.setString(2, "Test Name10");
+                try (PreparedStatement statement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS)) {
+                    statement.setString(1, newUser2.getEmail());
+                    statement.setString(2, newUser2.getName());
                     statement.executeUpdate();
+
+                    try (ResultSet keys = statement.getGeneratedKeys()) {
+                        if (keys.next()) {
+                            newUser2.setId(keys.getInt(1));
+                        }
+                    }
 
                 }
             });
